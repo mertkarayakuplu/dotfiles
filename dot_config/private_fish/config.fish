971f3883f57d809dd11c1f9fdc6c1f1236c9690c
Mert Karayakuplu <mertkarayakuplu@gmail.com>
2024-10-24 18:43:42 +0300

    update fish conf
    Signed-off-by: Mert Karayakuplu <mertkarayakuplu@gmail.com>

--- dot_config/private_fish/config.fish
+++ dot_config/private_fish/config.fish
@@ -1,3 +1,11 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+function fish_greeting
+    echo "Nothing in the world can take the place of persistence.";
+    echo "Talent will not; nothing is more common that unsuccessful individuals with talent.";
+    echo "Genius will not; unrewarded genius is almost a proverb. Education will not; the world is full of educated derelicts. Persistence and determination alone are omnipotent.";
+end
+
+
